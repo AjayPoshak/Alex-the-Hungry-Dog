abcb8f7de00a2e1f2993e4e86db9ef308a1755c4
Ajay Poshak <poshakajay@gmail.com>
2020-08-22 16:25:24 +0530

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header'
 import Grid from './components/Grid'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Grid N={8} />
     </div>
   );
