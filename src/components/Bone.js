088a764c29b56212447f2bd6efc36388e0f21f71
Ajay Poshak <poshakajay@gmail.com>
2020-08-22 21:34:32 +0530

    Add images for dog and bones

--- src/components/Bone.js
+++ src/components/Bone.js
@@ -5,9 +5,9 @@ import RottenMeatSVG from '../icons/rotten_meat.svg'
 const Bone = (props) => {
     const {isRotten = false} = props
     if(isRotten === true) {
-        return <img className="bone rotten" src={RottenMeatSVG} />
+        return <img className="bone rotten" src={RottenMeatSVG} alt="" />
     }
-    return <img className="bone" src={BoneSVG} />
+    return <img className="bone" src={BoneSVG} alt="" />
 }
 
 export default Bone
--- src/components/Dog.js
+++ src/components/Dog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DogSVG from '../icons/dog.svg'
 
 const Dog = ({direction}) => {
-    return <img className={`dog ${direction}`} src={DogSVG} />
+    return <img className={`dog ${direction}`} src={DogSVG} alt="" />
 }
 
 export default Dog
