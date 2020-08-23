3676f4eb8ca87ce99e4cb88102a062715400cc5b
AjayPoshak <poshakajay@gmail.com>
2020-08-23 06:39:18 +0000

    Deploying to gh-pages from  @ f4cce91a9151251ddf78a8f7bdd094b9ad243417 🚀

---
+++ precache-manifest.d462f4218e89a7a58ce70ee527a57115.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce75d9c7cbfa3da036ee44e06cb2215",
+    "url": "/alex-the-hungry-dog/index.html"
+  },
+  {
+    "revision": "d30ff7e256787308f524",
+    "url": "/alex-the-hungry-dog/static/css/main.182765d6.chunk.css"
+  },
+  {
+    "revision": "0388956d47466661affc",
+    "url": "/alex-the-hungry-dog/static/js/2.c6d4bf3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alex-the-hungry-dog/static/js/2.c6d4bf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d30ff7e256787308f524",
+    "url": "/alex-the-hungry-dog/static/js/main.3d3a705a.chunk.js"
+  },
+  {
+    "revision": "e6c258a3b4cdb1f29a65",
+    "url": "/alex-the-hungry-dog/static/js/runtime-main.414d3f36.js"
+  },
+  {
+    "revision": "7475f535d0030cbeabde5ea123f3eb35",
+    "url": "/alex-the-hungry-dog/static/media/bone.7475f535.svg"
+  },
+  {
+    "revision": "ae075a9097c2ff7ac2903abe8ad6cd80",
+    "url": "/alex-the-hungry-dog/static/media/dog.ae075a90.svg"
+  },
+  {
+    "revision": "bc3d20ac6dd392f6be28638f6576d27d",
+    "url": "/alex-the-hungry-dog/static/media/rotten_meat.bc3d20ac.svg"
+  }
+]);
