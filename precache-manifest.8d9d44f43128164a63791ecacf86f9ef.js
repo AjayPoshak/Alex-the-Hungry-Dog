d86e3081c0dd01d5388fd18ba1e50c6a36fd2ae0
AjayPoshak <AjayPoshak@users.noreply.github.com>
2020-08-23 14:05:42 +0000

    Deploying to gh-pages from  @ e6eee0dc2136d5dfe99752c7bb8ea72e26bc3ad1 🚀

---
+++ precache-manifest.8d9d44f43128164a63791ecacf86f9ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa119242d610c7badb1f7469c1b1be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82c3686dfb76231d32d",
+    "url": "/static/css/main.182765d6.chunk.css"
+  },
+  {
+    "revision": "0388956d47466661affc",
+    "url": "/static/js/2.c6d4bf3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6d4bf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82c3686dfb76231d32d",
+    "url": "/static/js/main.2a79c4ed.chunk.js"
+  },
+  {
+    "revision": "c443d864af4849590443",
+    "url": "/static/js/runtime-main.9a6b461c.js"
+  },
+  {
+    "revision": "7475f535d0030cbeabde5ea123f3eb35",
+    "url": "/static/media/bone.7475f535.svg"
+  },
+  {
+    "revision": "ae075a9097c2ff7ac2903abe8ad6cd80",
+    "url": "/static/media/dog.ae075a90.svg"
+  },
+  {
+    "revision": "bc3d20ac6dd392f6be28638f6576d27d",
+    "url": "/static/media/rotten_meat.bc3d20ac.svg"
+  }
+]);
