c31bdb1e0bff6756c331901cab2da0b57d1f046e
Ajay Poshak <poshakajay@gmail.com>
2020-08-29 21:07:28 +0530

    Format code and add game start UI

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,35 +1,46 @@
-import React from 'react'
+import React from "react";
 
-import Dog from './Dog'
-import Bone from './Bone'
-
+import Dog from "./Dog";
+import Bone from "./Bone";
+import GameStart from "../components/GameStart";
 
 const Grid = (props) => {
-    const {gridList, dogDirection} = props
-    return (
-        <section className="grid-container">
-            {gridList.map((row, index) => {
+  const { gridList, dogDirection, handlePlayClick, shouldStartGame } = props;
+  return (
+    <section className="grid-container">
+      {shouldStartGame === false ? <GameStart handlePlayClick={handlePlayClick} /> : null}
+      {gridList.map((row, index) => {
+        return (
+          <section className="row" key={index}>
+            {row.map((element, index) => {
+              if (element === "D") {
                 return (
-                    <section className="row" key={index}>
-                        {row.map((element, index) => {
-                            if(element === 'D') {
-                                return (<div className="grid-element" key={index}>
-                                    <Dog direction={dogDirection} />
-                                    </div>)
-                            }
-                            if(element === 'B') {
-                                return <div className="grid-element" key={index}><Bone isRotten={false}/></div>
-                            }
-                            if(element === 'R') {
-                                return <div className="grid-element" key={index}><Bone isRotten/></div>
-                            }
-                            return <span className="grid-element" key={index}></span>
-                        })}
-                    </section>
-                )
+                  <div className="grid-element" key={index}>
+                    <Dog direction={dogDirection} />
+                  </div>
+                );
+              }
+              if (element === "B") {
+                return (
+                  <div className="grid-element" key={index}>
+                    <Bone isRotten={false} />
+                  </div>
+                );
+              }
+              if (element === "R") {
+                return (
+                  <div className="grid-element" key={index}>
+                    <Bone isRotten />
+                  </div>
+                );
+              }
+              return <span className="grid-element" key={index}></span>;
             })}
-        </section>
-    )
-}
+          </section>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Grid
+export default Grid;
